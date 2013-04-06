df2a9e87f00a5a3c29c021bce655d7737df03065
Kris Leech <kris.leech@gmail.com>
2013-04-07 00:52:15 +0100

    reword and regroup specs to make reading easier

--- spec/lib/wisper_spec.rb
+++ spec/lib/wisper_spec.rb
@@ -5,7 +5,7 @@ describe Wisper do
   let(:publisher) { Object.new.extend(Wisper) }
 
   describe '.add_listener' do
-    it 'subscribes listener to all published events' do
+    it 'subscribes given listener to all published events' do
       listener.should_receive(:this_happened)
       listener.should_receive(:so_did_this)
 
@@ -15,46 +15,39 @@ describe Wisper do
       publisher.send(:broadcast, 'so_did_this')
     end
 
-    it 'subscribes listener to selected events' do
-      listener.should_receive(:this_happened)
-      listener.stub(:so_did_this)
-      listener.should_not_receive(:so_did_this)
+    describe ':on argument' do
+      it 'subscribes given listener to selected events' do
+        listener.should_receive(:this_happened)
+        listener.stub(:so_did_this)
+        listener.should_not_receive(:so_did_this)
 
-      listener.respond_to?(:so_did_this).should be_true
+        listener.respond_to?(:so_did_this).should be_true
 
-      publisher.add_listener(listener, :on => 'this_happened')
+        publisher.add_listener(listener, :on => 'this_happened')
 
-      publisher.send(:broadcast, 'this_happened')
-      publisher.send(:broadcast, 'so_did_this')
+        publisher.send(:broadcast, 'this_happened')
+        publisher.send(:broadcast, 'so_did_this')
+      end
     end
 
-    it 'does not receive events it can not respond to' do
-      listener.should_not_receive(:so_did_this)
-      listener.stub(:respond_to?, false)
+    describe ':with argument' do
+      it 'sets method to call listener with on event' do
+        listener.should_receive(:different_method).twice
 
-      listener.respond_to?(:so_did_this).should be_false
+        publisher.add_listener(listener, :with => :different_method)
 
-      publisher.add_listener(listener, :on => 'so_did_this')
-
-      publisher.send(:broadcast, 'so_did_this')
+        publisher.send(:broadcast, 'this_happened')
+        publisher.send(:broadcast, 'so_did_this')
+      end
     end
 
-    it 'subscriber is called with a different method' do
-      listener.should_receive(:different_method).twice
-
-      publisher.add_listener(listener, :with => :different_method)
-
-      publisher.send(:broadcast, 'this_happened')
-      publisher.send(:broadcast, 'so_did_this')
-    end
-
-    it 'adding listeners can be chained' do
+    it 'returns publisher so methods can be chained' do
       publisher.add_listener(listener, :on => 'so_did_this').should == publisher
     end
   end
 
-  describe 'Block listeners' do
-    it '.add_block_listener subscribes block to all events' do
+  describe '.add_block_listener' do
+    it 'subscribes given block to all events' do
       insider = double('insider')
       insider.should_receive(:it_happened)
 
@@ -65,34 +58,27 @@ describe Wisper do
       publisher.send(:broadcast, 'something_happened')
     end
 
-    it '.add_block_listener subscribes block to selected events' do
-      insider = double('insider')
-      insider.should_not_receive(:it_happened)
+    describe ':on argument' do
+      it '.add_block_listener subscribes block to an event' do
+        insider = double('insider')
+        insider.should_not_receive(:it_happened)
 
-      publisher.add_block_listener(:on => 'this_thing_happened') do
-        insider.it_happened
+        publisher.add_block_listener(:on => 'this_thing_happened') do
+          insider.it_happened
+        end
+
+        publisher.send(:broadcast, 'something_happened')
       end
-
-      publisher.send(:broadcast, 'something_happened')
     end
 
-    it '.add_block_listener can be chained' do
+    it 'returns publisher so methods can be chained' do
       publisher.add_block_listener(:on => 'this_thing_happened') do
       end.should == publisher
     end
+  end
 
-    it '.respond_to subscribes block to all events' do
-      insider = double('insider')
-      insider.should_receive(:it_happened)
-
-      publisher.respond_to(:something_happened) do
-        insider.it_happened
-      end
-
-      publisher.send(:broadcast, 'something_happened')
-    end
-
-    it '.on subscribes block to all events' do
+  describe '.on' do
+    it 'subscribes given block to an event' do
       insider = double('insider')
       insider.should_receive(:it_happened)
 
@@ -105,7 +91,16 @@ describe Wisper do
   end
 
   describe '.broadcast' do
-    describe 'event argument' do
+    it 'does not publish events which cannot be responded to' do
+      listener.should_not_receive(:so_did_this)
+      listener.stub(:respond_to?, false)
+
+      publisher.add_listener(listener, :on => 'so_did_this')
+
+      publisher.send(:broadcast, 'so_did_this')
+    end
+
+    describe ':event argument' do
       it 'is indifferent to string and symbol' do
         listener.should_receive(:this_happened).twice
 
