9eaddd0a6641561b96e937a8f0f741f2d4a999c2
Kris Leech <kris.leech@gmail.com>
2013-04-14 20:26:06 +0100

    tidy up spec

--- spec/lib/global_subscribers_spec.rb
+++ spec/lib/global_subscribers_spec.rb
@@ -1,22 +1,28 @@
 require 'spec_helper'
 
-describe Wisper do
+describe Wisper::GlobalListeners do
   let(:global_listener)  { double('listener') }
   let(:local_listener)   { double('listener') }
-  let(:publisher) { Object.new.extend(Wisper) }
+  let(:publisher)        { Object.new.extend(Wisper) }
 
-  it '.add_listener' do
-    Wisper::GlobalListeners.add_listener(global_listener)
-    global_listener.should_receive(:it_happened)
-    publisher.send(:broadcast, :it_happened)
-  end
+  describe '.add_listener' do
+    it 'adds given listener to every publisher' do
+      Wisper::GlobalListeners.add_listener(global_listener)
+      global_listener.should_receive(:it_happened)
+      publisher.send(:broadcast, :it_happened)
+    end
 
-  it 'mix and match local and global listeners' do
-    Wisper::GlobalListeners.add_listener(global_listener)
-    global_listener.should_receive(:it_happened)
-    local_listener.should_receive(:it_happened)
-    publisher.add_listener(local_listener)
-    publisher.send(:broadcast, :it_happened)
+    it 'works along side local listeners' do
+      # global listener
+      Wisper::GlobalListeners.add_listener(global_listener)
+
+      # local listener
+      publisher.add_listener(local_listener)
+
+      global_listener.should_receive(:it_happened)
+      local_listener.should_receive(:it_happened)
+
+      publisher.send(:broadcast, :it_happened)
+    end
   end
 end
-
