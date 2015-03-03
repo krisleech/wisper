1c267c1ede3d74ea622cfba6cfd6929128319c4e
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-03 17:07:28 +0300

    refactor Wisper::Registration on the base of Wisper::ValueObjects::Events

--- lib/wisper/registration/registration.rb
+++ lib/wisper/registration/registration.rb
@@ -4,39 +4,15 @@ module Wisper
   class Registration
     attr_reader :on, :listener
 
-    ALL = Object.new.freeze
-
     def initialize(listener, options)
       @listener = listener
-      @on       = stringify(options.fetch(:on, ALL))
+      @on = ValueObjects::Events.new options[:on]
     end
 
     private
 
     def should_broadcast?(event)
-      return true if on == ALL
-
-      case on.class.name
-      when 'String'
-        event == on
-      when 'Array'
-        on.include?(event)
-      when 'Regexp'
-        !!on.match(event)
-      else
-        raise ArgumentError, "#{on.class} not supported for `on` argument"
-      end
-    end
-
-    def stringify(on)
-      case on.class.name
-      when 'Symbol'
-        on.to_s
-      when 'Array'
-        on.map(&:to_s)
-      else
-        on
-      end
+      on.include? event
     end
   end
 end
